fc82a8cf5e766cabb6faa672fe55539875ce095e
Łukasz Twarogowski <twarogowski@gmail.com>
2011-06-12 16:48:03 +0200

    added test page for datepicker widget.

--- public/javascripts/angular-formatters.js
+++ public/javascripts/angular-formatters.js
@@ -3,27 +3,25 @@
 angular.formatter('nextYear', {
 	parse: function(value){
 		var v = new Date(value.valueOf());
-		var y = v.getFullYear();
-		y--;
-		v.setYear(y);
+		v.setYear(v.getFullYear()--);
 		return v;
 	},
 	format: function(value){
 		var v = new Date(value.valueOf());
-		var y = v.getFullYear();
-		y++;
-		v.setYear(y);
+		v.setYear(v.getFullYear()++);
 		return v;
 	}
 });
 
 // for testing only: displays in a bound datepicker a date with a specified amount of months added to it
-angular.formatter('addMonths', {
-	parse: function(value, months){
-		return value.setMonth(value.getMonth() + months);
+angular.formatter('addDate', {
+	parse: function(value, years, months, days){
+		var v = new Date(value.getFullYear() - parseFloat(years || 0), value.getMonth() - parseFloat(months || 0), value.getDate() - parseFloat(days || 0));
+		return v
 	},
-	format: function(value, months){
-		return value.setMonth(value.getMonth() - months);
+	format: function(value, years, months, days){
+		var v = new Date(value.getFullYear() + parseFloat(years || 0), value.getMonth() + parseFloat(months || 0), value.getDate() + parseFloat(days || 0));
+		return v
 	}
 });
 
